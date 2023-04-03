795cf80c161d96e46f3aa15bbb1779792753edcf
Amaar Hassnain <93473921+Amaar015@users.noreply.github.com>
2023-04-03 12:28:11 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const port = 8000;
-const product = require('./data/products')
+const products = require('./data/products')
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
 app.get('/product', (req, res) => {
+    res.json(products)
+})
+app.get('/product/:id', (req, res) => {
+    const product = products.find((p) => p._id === req.params.id)
     res.json(product)
 })
-
 app.listen(port, () => {
     console.log(`listening from the port ${port}`)
 })
