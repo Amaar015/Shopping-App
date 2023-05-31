4bbc38d3135ac01cc0d962b1536dc18399eca56c
Amaar Hassnain <93473921+Amaar015@users.noreply.github.com>
2023-05-31 09:11:41 +0500

    user models and global routes updated

--- backend/models/userModels.js
+++ backend/models/userModels.js
@@ -24,6 +24,10 @@ const userSchema = mongoose.Schema({
         type: String,
         required: true,
     },
+    question: {
+        type: String,
+        required: true,
+    },
     role: {
         type: Number,
         default: 0,
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,7 +2,9 @@
 // import an express
 const express = require('express');
 // create a router object
-const { registerController, LoginController } = require('../controller/authController.js')
+const { registerController, LoginController, authController, forgetPasswordController } = require('../controller/authController.js');
+const { requireSignIn } = require('../midelwares/authMiddleware.js');
+const loginMiddleware = require('../midelwares/loginMiddleware.js');
 
 const router = express.Router();
 
@@ -14,5 +16,12 @@ router.post('/register', registerController)
 // Login routes post methode
 
 router.post('/login', LoginController)
+// auth
+router.post('/getUserData', loginMiddleware, authController)
+
+// Forget password || post
+router.post('/forget-password', forgetPasswordController)
+
+
 
 module.exports = router;
